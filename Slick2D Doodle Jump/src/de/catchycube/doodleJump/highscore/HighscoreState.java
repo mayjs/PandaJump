ff7380bf0282e37cdef9b1457513d32e007536a8
Kemiren <thekemiren@googlemail.com>
2014-01-05 16:47:22 +0100

    Highscore State now has a neat moving color gradient layed over the text

--- Slick2D Doodle Jump/src/de/catchycube/doodleJump/highscore/HighscoreState.java
+++ Slick2D Doodle Jump/src/de/catchycube/doodleJump/highscore/HighscoreState.java
@@ -7,9 +7,13 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.Font;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.fills.GradientFill;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -23,37 +27,155 @@ public class HighscoreState extends BasicGameState{
 	private int highlight=-1;
 	private Color normalColor=Color.white,highlightColor=Color.red;
 	private StateBasedGame game;
-	private Font font;
+	private Font font, fontHeader;
+	private String headerText="Highscore";
+	private Image bufferImg;
+	private Graphics imgGraphics;
+	private GradientFill overlay;
+	private Rectangle fillingRect=new Rectangle(0, 0, 0, 0);
+	private float m, xpms=0.1f;
+	private float ypms=0.3f, startFac=-1f, endFac=1f;
+	
+	private GameContainer con;
 	
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
 		this.game = game;
 		font = new TrueTypeFont(new java.awt.Font("verdana", java.awt.Font.BOLD, 20), true);
+		fontHeader = new TrueTypeFont(new java.awt.Font("verdana", java.awt.Font.BOLD, 45), true);
 		
 		highscore = Highscore.load(Highscore.standardFileName);
 		if(highscore==null){
 			highscore = new Highscore();
-		}	
+		}
+		
+		bufferImg = new Image(container.getWidth(), container.getHeight());
+		imgGraphics = bufferImg.getGraphics();
+		
+		overlay = new GradientFill(0,1, Color.red, 1, 0, Color.orange.darker(0.25f));
+		overlay.setLocal(false);
+		
+		calcFillingRect(container);
+		con = container;
+		
+		
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
+		Graphics.setCurrent(imgGraphics);
+		imgGraphics.clearAlphaMap();
+		imgGraphics.setDrawMode(Graphics.MODE_NORMAL);
+		
 		int x=container.getWidth()/2;
 		int y=(container.getHeight() - (highscore.getNumberOfEntries() * font.getLineHeight()))/2;
 		
 		List<HighscoreEntry> entries = highscore.getEntries();
 		for(int i = 0; i < entries.size(); i++){
-			String s = entries.get(i).toString();
-			font.drawString(x-font.getWidth(s)/2,y+i*font.getLineHeight(), s, i==highlight?highlightColor:normalColor);
+			String s = (i+1)+". " + entries.get(i).toString();
+			if(i==highlight){
+				s = "> " + s + " <";
+			}
+			font.drawString(x-font.getWidth(s)/2,y+i*font.getLineHeight(), s, /*i==highlight?highlightColor:*/normalColor);
 		}
+		
+		int hx = (container.getWidth()-fontHeader.getWidth(headerText))/2;
+		fontHeader.drawString(hx, y /2/*- fontHeader.getLineHeight() - 15*/, headerText);
+		
+		imgGraphics.setDrawMode(Graphics.MODE_COLOR_MULTIPLY);
+		imgGraphics.fill(fillingRect, overlay);
+		
+		imgGraphics.flush();
+		
+		Graphics.setCurrent(g);
+		bufferImg.draw();
+		
+		//DEBUG: Show gradient start and end point
+//		g.setColor(Color.green);
+//		g.fillRect(overlay.getStart().x -5, overlay.getStart().y -5, 10, 10);
+//		g.setColor(Color.blue);
+//		g.fillRect(overlay.getEnd().x -5, overlay.getEnd().y -5, 10, 10);
+	}
+	
+	private void calcFillingRect(GameContainer container){
+		int x=container.getWidth()/2;
+		int y=(container.getHeight() - (highscore.getNumberOfEntries() * font.getLineHeight()))/2;
+		
+		int minY = y/2; int maxY = y+highscore.getNumberOfEntries()*font.getLineHeight();
+		int minX = 0; int maxX = 0;
+		minX = (container.getWidth()-fontHeader.getWidth(headerText))/2; maxX = minX + fontHeader.getWidth(headerText);
+		List<HighscoreEntry> entries = highscore.getEntries();
+		for(int i = 0; i < entries.size(); i++){
+			String s = (i+1)+". " + entries.get(i).toString();
+			if(i==highlight){
+				s = "> " + s + " <";
+			}
+			int ex = x-font.getWidth(s)/2;
+			if(ex < minX){
+				minX = ex;
+				maxX = minX + font.getWidth(s);
+			}
+		}
+		
+		fillingRect.setBounds(minX, minY, maxX - minX, maxY - minY);
+		
+		overlay.setStart(minX, maxY);
+		overlay.setEnd(maxX, minY);
+		
+		m = fillingRect.getHeight() / fillingRect.getWidth();
+		
+		overlay.setEnd(maxX, minY);
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta)
 			throws SlickException {
+		Vector2f start = overlay.getStart();
+		Vector2f end = overlay.getEnd();
 		
+//		float dx = delta * xpms;
+//		float dy = m * dx;
+		
+		float dy = delta * ypms;
+		
+////		
+////		start.x += dx;
+////		start.y -= dy;
+////		
+////		if(start.x > fillingRect.getMaxX()){
+////			start.x -= fillingRect.getWidth();
+////			start.y += fillingRect.getHeight();
+////		}
+////		
+//		end.x += dx;
+//		end.y -= dy;
+//		
+//		if(end.x > fillingRect.getMaxX()){
+////			end.x -= fillingRect.getWidth();
+////			end.y += fillingRect.getHeight();
+//			xpms *= -1;
+//		}
+//		else if(end.x < fillingRect.getMinX()){
+//			xpms *= -1;
+//		}
+		
+		start.y += dy * startFac;
+		end.y += dy*endFac;
+		if(start.y < fillingRect.getMinY()){
+			startFac = 1;
+		} else if(start.y > fillingRect.getMaxY()){
+			startFac = -1;
+		}
+		if(end.y < fillingRect.getMinY()){
+			endFac = 1;
+		} else if(end.y > fillingRect.getMaxY()){
+			endFac = -1;
+		}
+		
+		overlay.setStart(start);
+		overlay.setEnd(end);
 	}
 
 	@Override
@@ -79,10 +201,13 @@ public class HighscoreState extends BasicGameState{
 	}
 	
 	public int add(String name, int score){
-		return highscore.add(name, score);
+		int r = highscore.add(name, score);
+		calcFillingRect(con);
+		return r;
 	}
 	
 	public void highlight(int i){
 		highlight = i;
+		calcFillingRect(con);
 	}
 }
